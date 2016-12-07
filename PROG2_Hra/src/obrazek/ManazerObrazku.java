eb178ffdb97b140c0760bc7acb0ca8e1d4ae3f98
nekolda1 <nekolda1@J08-24.uhk.cz>
2016-12-07 11:56:26 +0100

    manazerObrazku

---
+++ PROG2_Hra/src/obrazek/ManazerObrazku.java
@@ -0,0 +1,18 @@
+package obrazek;
+
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ManazerObrazku {
+
+	private Map<String, BufferedImage> mapaObr;
+	private ZdrojObrazku zo;
+	
+	public ManazerObrazku(ZdrojObrazku zo) {
+		mapaObr = new HashMap<>();
+		this.zo = zo;
+		this.zo.naplnMapu();
+	}
+	
+}
